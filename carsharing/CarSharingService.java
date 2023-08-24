cf77a5ea2c31a85d9f7c47c324a4291189963a91
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-08-24 23:06:07 +0200

    feat: Hyperskill project Car Sharing. Stage 2/4: Companies
    - Menu per stage 2 implemented
    
    ref #03

--- carsharing/CarSharingService.java
+++ carsharing/CarSharingService.java
@@ -1,7 +1,5 @@
 package carsharing;
 
-import carsharing.persistance.Company;
-import carsharing.persistance.CompanyDao;
 import carsharing.persistance.DataBaseClient;
 
 import java.sql.SQLException;
@@ -23,43 +21,32 @@ public class CarSharingService {
 //        Class.forName(DataBaseClient.JDBC_DRIVER);
 //        CompanyDao companyDao = new CompanyDao();
 //        companyDao.add(new Company(1, "My fantastic Company"));
-//        companyDao.add(new Company(1, "My fantastic Srompany"));
-//        companyDao.add(new Company(1, "My fantastic Death"));
-//        companyDao.add(new Company(1, "My fantastic Inne"));
+//        companyDao.add(new Company(1, "My fantastic Company 2"));
+//        companyDao.add(new Company(1, "My fantastic Company 3"));
 
         mainMenu();
     }
 
     private void mainMenu() {
         Menu menu = new Menu();
-        menu.setTitle("*** My Main Menu ***");
-        menu.addItem(new MenuItem("Option A", this, "subMenuA"));
-        menu.addItem(new MenuItem("Option B", this, "subMenuB"));
-        menu.addItem(new MenuItem("Option C", this, "performOptionC"));
+        menu.addItem(new MenuItem("Log in as a manager", this, "subMenuA"));
         menu.execute();
     }
 
     public void subMenuA() {
         Menu menu = new Menu();
-        menu.setTitle("*** Sub Menu A ***");
-        menu.addItem(new MenuItem("Option Aa"));
-        menu.addItem(new MenuItem("Option Ab"));
+        menu.addItem(new MenuItem("Company List", this, "getCompanyList"));
+        menu.addItem(new MenuItem("Create a company", this, "createCompany"));
         menu.execute();
     }
 
-    /* This menu has no menu items but will still generate a single exit option. */
-    public void subMenuB() {
-        Menu menu = new Menu();
-        menu.setTitle("*** Sub Menu B ***");
-        menu.execute();
+    public void getCompanyList() {
+        System.out.println("\nCompany list:");
+        System.out.println("\nList of companies (TODO)");
     }
 
-    /* Added a confirmation request here to demonstrate how it could be used. */
-    public void performOptionC() {
-        boolean confirm = ConsoleUtils.requestConfirmation();
-        if (confirm)
-            System.out.println("\nDo Option C...");
-        else System.out.println("\nOption C cancelled!");
-        ConsoleUtils.pauseExecution();
+    public void createCompany() {
+        System.out.println("\nEnter the company name:");
+        System.out.println("\nCompany creation (TODO)");
     }
 }
